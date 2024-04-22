425a0834be765935fe4ee695c311c94eef0eb6f4
iiotakamasa <tiio@mail.doshisha.ac.jp>
2024-04-22 16:53:24 +0900

    Update sample3.py
    Changed to use azure speech recognition.

--- sample3.py
+++ sample3.py
@@ -1,56 +1,56 @@
 from robottools import RobotTools
 import openai
-import speech_recognition as sr
+import azure.cognitiveservices.speech as speechsdk
 import time
 
-rt = RobotTools('192.168.11.41', 22222)
+rt = RobotTools("192.168.11.41", 22222)
 
-# OpenAI APIキーをセットアップ
-openai.api_key = 'APIキーを入力'
+# OpenAIのAPIのセットアップ
+openai.api_key = "APIキーを入力"
+model_engine = "gpt-4-turbo"
 
-# OpenAIのGPTに接続
-model_engine = 'gpt-3.5-turbo'
+# MS Azureの音声認識器を作成
+speech_key, service_region = "APIキーを入力", "japanwest"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+speech_config.speech_recognition_language="ja-JP"
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-# 音声認識器を作成
-r = sr.Recognizer()
-
-# ChatGPTへのリクエストに含めるパラメータ
-params = [
-    {'role': 'system', 'content': 'あなたはユーザーの雑談相手です。'}
+prompt = [
+    {'role': 'system', 'content': 'あなたはユーザーの雑談相手です。'},
+    {'role': 'system', 'content': '返事は短めに二文までにしてください。'}
 ]
 
-# マイクから音声を連続認識
-with sr.Microphone() as source:
-    r.adjust_for_ambient_noise(source)  # ノイズ対策（オプション）
-    while True:
-        # print('何か話してください...')
-        
-        audio = r.listen(source)
-        
-        try:
-            user_input = r.recognize_google(audio, language='ja-JP')
-            print('USER: ' + user_input)
-        except sr.UnknownValueError:
-            # print('Google Speech Recognition could not understand audio')
-            continue
-        except sr.RequestError as e:
-            # print('Could not request results from Google Speech Recognition service; {0}'.format(e))
-            continue
+while True:
+    # 音声認識開始
+    result = speech_recognizer.recognize_once()
 
-        # ユーザーの入力を送信し、返答を取得
-        params.append({'role': 'user', 'content': user_input})
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=params
+    # 音声認識結果（ユーザの発話）の処理
+    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("USER:", result.text)
+        # プロンプトにユーザの発話を追加
+        prompt.append({'role': 'user', 'content': result.text})
+        # ユーザの発話をChatGPTに送信、生成された応答文を取得
+        response = openai.chat.completions.create(
+            model=model_engine,
+            messages=prompt
         )
         message = response.choices[0].message.content
-        params.append({'role': 'assistant', 'content': message})
-        print('ROBOT: ' + message)
-
         # ロボットに返答を発話させる
         d = rt.say_text(message)
         m = rt.make_beat_motion(d, speed=1.5)
         rt.play_motion(m)
-
+        # プロンプトにロボット（Assistant）の発話を追加
+        prompt.append({"role": "assistant", "content": message})
+        print("ROBOT:", message)
         # 発話中は音声認識を止める
         time.sleep(d)
+    elif result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(result.no_match_details))
+    elif result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+
+    if result.text.startswith('終了'):
+        break
